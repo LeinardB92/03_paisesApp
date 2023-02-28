ef515a5cc3f757629f12b2dfa2221d1fbf06b7eb
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-02-27 21:00:32 -0600

    RouterModule, Rutas principales en la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     PaisModule,
     SharedModule
   ],
