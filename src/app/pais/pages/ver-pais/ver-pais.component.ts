cac385c5c4fa04cd044046858987325714ef9648
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-03-13 21:34:05 -0600

    Ver paises de forma independiente, creación de método .getPaisPorAlpha

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html'
 })
-export class VerPaisComponent {
+export class VerPaisComponent implements OnInit {
+  
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+    ){}
+  
+  ngOnInit(): void {
+    // Detectar cambios en la ruta URL de la aplicación.
+    this.activatedRoute.params.subscribe(params => {
+      console.log(params['id']);
+
+      this.paisService.getPaisPorAlpha(params['id']).subscribe(pais => {
+        console.log(pais);
+      });
+    })
+  }
+
 
 }
