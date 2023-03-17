39fba619b65a087c68a5f2da83c43c846a0b6ac1
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-03-16 20:45:29 -0600

    uso de [ngClass]

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -2,8 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
-  templateUrl: './por-region.component.html'
+  templateUrl: './por-region.component.html',
+  styles:[`
+    button{
+      margin-right: 5px;
+    }
+  `]
 })
 export class PorRegionComponent {
 
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = "";
+
+  activarRegion( region: string){
+    this.regionActiva = region;
+
+    //To Do: Hacer llamado al servicio para traer los paises por la región señalada en el parámetro.
+  }
 }
