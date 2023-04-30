9a4183769d77991d7a906f59e2b772ada1c09d08
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-04-29 19:53:59 -0600

    Buscar por región - optimizado

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
+type Region = 'africa' | 'americas' | 'asia' | 'europe' | 'oceania';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
@@ -13,15 +14,16 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorRegionComponent {
 
-  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regiones : Region[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
   regionActiva: string = "";
   paisesRegion: Country[] = [];
+  isLoading : boolean = false;
 
   constructor(private paisService: PaisService){}
 
   getClaseCSS(region: string){
     return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
-  }  
+  }
 
   activarRegion( region: string){
 
@@ -29,10 +31,12 @@ export class PorRegionComponent {
 
     this.regionActiva = region;
     this.paisesRegion = [];
+    this.isLoading = true;
 
     this.paisService.buscarRegion(this.regionActiva)
-    .subscribe({next:(paisesRegion: Country[]) => {
-      this.paisesRegion = paisesRegion;
+      .subscribe({next:(paisesRegion: Country[]) => {
+        this.paisesRegion = paisesRegion;
+        this.isLoading = false;
       }})
   }
 }
